3121a352d48cc1d05a07ba8f859703d5cfe55b64
zhouqixian <553418768@qq.com>
2016-05-31 15:15:09 +0800

    upadte

---
+++ src/com/example/myticket/activities/MovieInfoActivity.java
@@ -0,0 +1,99 @@
+package com.example.myticket.activities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.example.myticket.R;
+import com.example.myticket.R.layout;
+import com.example.myticket.db.DataBaseHelper;
+import com.example.myticket.db.MyApplication;
+import com.example.myticket.entities.Cinema;
+import com.example.myticket.entities.Location;
+import com.example.myticket.entities.Movie;
+import com.example.myticket.entities.ProductDescription;
+import com.example.myticket.entities.ScreeningRoom;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class MovieInfoActivity extends Activity {
+	private TextView _name_tv, _point_tv, _tag_tv, _language_tv, _duration_tv, _director_tv;
+	private TextView _actors_tv, _plot_tv;
+	private ImageView _img, _back;
+	private Button _reserve_btn;
+	private MyApplication _application;
+	private DataBaseHelper dbHandler;
+	private Movie movie;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_movie_info);
+		initView();
+		initEvent();
+	}
+	private void initView() {
+		_application = (MyApplication)getApplication();
+		dbHandler = DataBaseHelper.getInstance(this);
+		_name_tv = (TextView)findViewById(R.id.movie_info_name);
+		_point_tv = (TextView)findViewById(R.id.movie_info_point);
+		_tag_tv = (TextView)findViewById(R.id.movie_info_tag);
+		_language_tv = (TextView)findViewById(R.id.movie_info_language);
+		_duration_tv = (TextView)findViewById(R.id.movie_info_duration);
+		_director_tv = (TextView)findViewById(R.id.movie_info_director);
+		_actors_tv = (TextView)findViewById(R.id.movie_info_actors);
+		_plot_tv = (TextView)findViewById(R.id.movie_info_plot);
+		_img = (ImageView)findViewById(R.id.movie_info_img);
+		_back = (ImageView)findViewById(R.id.movie_info_back);
+		_reserve_btn = (Button)findViewById(R.id.movie_info_reserve);
+		movie = (Movie)getIntent().getSerializableExtra("movie");
+		if (movie != null) {
+			_img.setImageResource(_application.getPhotoMap().get(movie.getPhoto()));
+			_name_tv.setText(movie.getMovie_name());
+			_point_tv.setText(Float.toString(movie.getPoint()));
+			_tag_tv.setText(movie.getTag());
+			_language_tv.setText(movie.getLanguages());
+			_duration_tv.setText(Integer.toString(movie.getDuration()));
+			_director_tv.setText(movie.getDirector());
+			_actors_tv.setText(movie.getDirector());
+			_plot_tv.setText(movie.getDescription());
+		}
+	}
+	private void initEvent() {
+		_back.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+		_reserve_btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MovieInfoActivity.this, ProductActivity.class);
+				intent.putExtra("movie", movie);
+				startActivity(intent);
+			}
+		});
+	}
+	
+}
---
+++ src/com/example/myticket/activities/ProductActivity.java
@@ -0,0 +1,115 @@
+package com.example.myticket.activities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.example.myticket.R;
+import com.example.myticket.R.layout;
+import com.example.myticket.db.DataBaseHelper;
+import com.example.myticket.db.MyApplication;
+import com.example.myticket.entities.Cinema;
+import com.example.myticket.entities.Location;
+import com.example.myticket.entities.Movie;
+import com.example.myticket.entities.ProductDescription;
+import com.example.myticket.entities.ScreeningRoom;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class ProductActivity extends Activity {
+	private SimpleAdapter simpleAdapter;
+	private List<Map<String, Object>> _listItems;
+	private ListView _pd_lv;
+	private MyApplication _application;
+	private DataBaseHelper dbHandler;
+	private Movie movie;
+	private ImageView _back;
+	private ArrayList<ProductDescription> _products;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_product);
+		initView();
+		setAdapter();
+		initEvent();
+	}
+	private void initView() {
+		_application = (MyApplication)getApplication();
+		dbHandler = DataBaseHelper.getInstance(this);
+		movie = (Movie)getIntent().getSerializableExtra("movie");
+		_pd_lv = (ListView)findViewById(R.id.product_list);
+		_back = (ImageView)findViewById(R.id.product_back);
+	}
+	private void initEvent() {
+		_back.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+		_pd_lv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(ProductActivity.this, ReserveActivity.class);
+				intent.putExtra("product", _products.get(arg2));
+				startActivity(intent);
+			}
+		});
+	}
+	private void setAdapter() {
+		if (movie == null) return;
+		_products = dbHandler.queryProductDescriptionsByMovie(movie.getMovie_id());
+		_listItems = new ArrayList<Map<String,Object>>();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd HH:mm");
+		SimpleDateFormat sdf1 = new SimpleDateFormat("HH:mm");
+		for (ProductDescription i : _products) {
+			Cinema cinema = dbHandler.queryCinema(i.getCinema_id());
+			ScreeningRoom screeningRoom = dbHandler.queryScreeningRoom(i.getScreening_room_id());
+			if (cinema != null && screeningRoom != null) {
+				Location location = dbHandler.queryLocation(cinema.getDist_code());
+				if (location != null) {
+					Map<String,Object> item = new HashMap<String, Object>();
+					item.put("cinema_name", cinema.getCinema_name());
+					item.put("cinema_address", location.getAddress_name());
+					item.put("type", i.getType());
+					item.put("screening_room", screeningRoom.getScreening_room_name());
+					Date start = i.getStartTime();
+					Calendar calendar = Calendar.getInstance();
+					calendar.setTime(start);
+					calendar.add(Calendar.MINUTE, movie.getDuration());
+					Date end = calendar.getTime();
+					item.put("start_time", sdf.format(start));
+					item.put("end_time", sdf1.format(end));
+					item.put("price", Float.toString(i.getPrice()));
+					_listItems.add(item);
+				}
+			}
+		}
+		if (!_listItems.isEmpty()) {
+			simpleAdapter = new SimpleAdapter(this, _listItems, R.layout.products_list_item, 
+					new String[]{"cinema_name", "cinema_address", "type", "screening_room", "start_time", "end_time", "price"}, 
+					new int[]{R.id.product_list_cinema_name, R.id.product_list_address, R.id.product_list_type, R.id.product_list_screening_room, R.id.product_list_start_time, R.id.product_list_end_time, R.id.product_list_price});
+			_pd_lv.setAdapter(simpleAdapter);
+		}
+	}
+}
